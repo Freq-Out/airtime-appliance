ff603c9c4c50f62937793e01465e1f33a7ee5153
Stephane Jourdan <fasten@gmail.com>
2013-10-05 14:50:31 +0200

    PostgreSQL 9.3 support

--- roles/db.rb
+++ roles/db.rb
@@ -2,14 +2,23 @@ name "db"
 description "Database Server"
 
 run_list(
-  "recipe[database]",
-  "recipe[postgresql]",
-  "recipe[postgresql::ruby]",
-  "recipe[postgresql::server]"
+  "recipe[postgresql::server]",
+  "recipe[database::postgresql]",
+  "recipe[postgresql::client]",
 )
 
 override_attributes(
 "postgresql" => {
-    "initdb_locale"      => "en_US.UTF-8"
+    "initdb_locale"     => "en_US.UTF-8",
+    "enable_pgdg_apt"   => true,
+    "version"           => "9.3",
+    "server" => {
+      "packages"      => ["postgresql-9.3"],
+      "service_name"  => "postgresql"
+    },
+    "client" => {
+      "packages" => ["postgresql-client-9.3", "postgresql-contrib-9.3", "postgresql-server-dev-9.3"]
+    }
   }
 )
+
