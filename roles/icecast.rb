941de930b4155d90a9bdf0f7991cb87f15edbf60
Stephane Jourdan <fasten@gmail.com>
2013-10-19 21:37:42 +0200

    Override icecast2 authentication defaults

--- roles/icecast.rb
+++ roles/icecast.rb
@@ -4,3 +4,14 @@ description "Install Icecast Server"
 run_list(
   "recipe[icecast2]"
 )
+
+override_attributes(
+"icecast2" => {
+    "authentication" => {
+      "source-password"  => "hackme",
+      "relay-password"  => "hackme",
+      "admin-user"      => "admin",
+      "admin-password"  => "hackme"
+    }
+  }
+)
