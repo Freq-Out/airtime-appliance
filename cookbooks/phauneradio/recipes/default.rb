2bd42e64de1fd92b2195c41bce1efe137a959477
Stephane Jourdan <fasten@gmail.com>
2013-10-20 14:16:56 +0200

    adds phauneradio cookbook from upstream

---
+++ cookbooks/phauneradio/recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: phauneradio
+# Recipe:: default
+#
+# Copyright 2013, Freq-Out
+#
---
+++ cookbooks/phauneradio/recipes/ladspa-plugins.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: phauneradio
+# Recipe:: ladspa-plugins
+#
+# Copyright 2013, Freq-Out
+#
+
+# Make sure we install our beloved audio plugins
+
+package "calf-plugins"
