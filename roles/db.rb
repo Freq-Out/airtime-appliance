671d95ccdddc78c7e485eb28fc7b23d9612e052d
Stephane Jourdan <fasten@gmail.com>
2013-10-29 19:18:19 +0100

    move back to standard postgresql 9.1
    Fixes and closes #21

--- roles/db.rb
+++ roles/db.rb
@@ -9,16 +9,7 @@ run_list(
 
 override_attributes(
 "postgresql" => {
-    "initdb_locale"     => "en_US.UTF-8",
-    "enable_pgdg_apt"   => true,
-    "version"           => "9.3",
-    "server" => {
-      "packages"      => ["postgresql-9.3"],
-      "service_name"  => "postgresql"
-    },
-    "client" => {
-      "packages" => ["postgresql-client-9.3", "postgresql-contrib-9.3", "postgresql-server-dev-9.3"]
-    }
+    "initdb_locale"     => "en_US.UTF-8"
   }
 )
 
